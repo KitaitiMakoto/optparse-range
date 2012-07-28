c8add233f7387a63bbcea1e63e0c076c4c48a1b1
KITAITI Makoto <KitaitiMakoto@gmail.com>
2012-07-29 00:48:52 +0900

    Make end point optional

--- lib/optparse/range.rb
+++ lib/optparse/range.rb
@@ -5,7 +5,7 @@ class OptionParser
   # --page=24-32,34-62 # not implemented for the first
   # ignore negative value for the first implementation
   decimal = '\d+(?:_\d+)*'
-  DecimalRange = /#{decimal}\-#{decimal}/
+  DecimalRange = /#{decimal}(?:\-#{decimal})/io
   accept DecimalRange do |range,|
     return unless range
     terms = range.split('-')

d6df1e0807d139371e48f0cbc1e0a038788ff1c0
KITAITI Makoto <KitaitiMakoto@gmail.com>
2012-07-28 23:35:31 +0900

    Add DecimalRange

--- lib/optparse/range.rb
+++ lib/optparse/range.rb
@@ -1,7 +1,16 @@
-require "optparse-range/version"
+require 'optparse'
 
-module Optparse
-  module Range
-    # Your code goes here...
+class OptionParser
+  # --port=8080-8082
+  # --page=24-32,34-62 # not implemented for the first
+  # ignore negative value for the first implementation
+  decimal = '\d+(?:_\d+)*'
+  DecimalRange = /#{decimal}\-#{decimal}/
+  accept DecimalRange do |range,|
+    return unless range
+    terms = range.split('-')
+    raise AmbiguousArgument if terms.length > 2
+    terms << terms.first if terms.length == 1
+    Range.new *terms.map(&:to_i)
   end
 end
